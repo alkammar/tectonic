db6b9238bfa2aecb10fb384ad4713a36aded800a
kammar <kamma@eg.ibm.com>
2017-11-10 10:02:26 +0400

    documentation

--- tectonic/src/main/java/com/morkim/tectonic/Result.java
+++ tectonic/src/main/java/com/morkim/tectonic/Result.java
@@ -1,7 +1,7 @@
 package com.morkim.tectonic;
 
 /**
- * The base class for a request to be sent to a use case
+ * The base class for a result returned to subscribers by a use case
  */
 public class Result {
 
